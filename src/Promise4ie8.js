6da56661b0a4fb12d1dda9b08387c8eeb4f301ea
Sun <sunpeiqian@che001.com>
2016-11-24 14:20:07 +0800

    -

--- src/Promise4ie8.js
+++ src/Promise4ie8.js
@@ -25,37 +25,3 @@ var Promise4ie8 = function() {
 		};
 	};
 }();
-
-function f1() {
-	var p = new Promise4ie8(function(resolve, reject) {
-		setTimeout(function() {
-			resolve('异步返回数据1');
-		}, 1000);
-	});
-	return p;
-}
-function f2() {
-	var p = new Promise4ie8(function(resolve, reject) {
-		setTimeout(function() {
-			resolve('异步返回数据2');
-		}, 1000);
-	});
-	return p;
-}
-function f3() {
-	var p = new Promise4ie8(function(resolve, reject) {
-		setTimeout(function() {
-			resolve('异步返回数据3');
-		}, 1000);
-	});
-	return p;
-}
-
-f1().then(function(data) {
-	console.log(data);
-	return f2();
-}).then(function(data) {
-	console.log(data);
-	return f3();
-});
-
